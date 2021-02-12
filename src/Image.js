fd7bce5ed4a93d50dba3cfb76c52a2753c7959c9
Jordan Phan <JordanJPhan@gmail.com>
2021-02-11 17:40:48 -0800

    minor fix

--- src/Image.js
+++ src/Image.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import './App.css'
-const Image = ({image}) => {
-    return (
-        <div className="imageContainer">
-            <img 
-                className="imageElement"
-                src={image.download_url} />
-            {image.author}
-        </div>
-    )
-}
+import React from "react";
+import "./App.css";
+const Image = ({ image }) => {
+  const deleteImage = () => {
+    let imageContainer = document.getElementById(`imageElement-${image.id}`);
+    imageContainer.style.display = "none";
+  };
+  return (
+    <div className="imageContainer" id={`imageElement-${image.id}`}>
+      <img className="imageElement" src={image.download_url} />
+      {image.author}
+      <div onClick={deleteImage} className="delete-button">
+        {" "}
+        X{" "}
+      </div>
+    </div>
+  );
+};
 
-export default Image
+export default Image;
